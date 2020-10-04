a4f6a08fa7b01db845abe19465b31486dca06f00
Matias Perez Hughes <hughesmatias@gmail.com>
2020-10-04 20:22:58 -0300

    second test- validation add new item in list

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('validacion de titulo', () => {
   const { getByText } = render(<App />);
-  getByText("TODOS");
+  getByText('TODOS');
+});
+
+test('validacion usuario agregando en la lista', () => {
+  const { getByLabelText, getByText } = render(<App />);
+  const primerElemento = 'Prueba 123';
+
+  const input = getByLabelText('Que necesitas?');
+  fireEvent.change(input, {
+    target: {
+      value: primerElemento,
+    },
+  });
+
+  fireEvent.click(getByText('Agregar #1'));
+
+  getByText(primerElemento);
 });
