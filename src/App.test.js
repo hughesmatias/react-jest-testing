26cae12d0c9cfb7d768b62f9bd39d1da7f68dc2b
Matias Perez Hughes <hughesmatias@gmail.com>
2020-10-04 19:49:43 -0300

    test title validation

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('validacion de titulo', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  getByText("TODOS");
 });
