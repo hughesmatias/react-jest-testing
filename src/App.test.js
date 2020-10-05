c82800da6e9e565704ed8b36c5fc9c15767daf37
Matias Perez Hughes <hughesmatias@gmail.com>
2020-10-04 21:09:20 -0300

    test - validation input is changing

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('validacion de titulo', () => {
@@ -21,4 +22,13 @@ test('validacion usuario agregando en la lista', () => {
   fireEvent.click(getByText('Agregar #1'));
 
   getByText(primerElemento);
+  fireEvent.click(getByText('Agregar #2'));
+});
+
+// otra funcion propia de testing library
+test('tipeo en el form para agregar en la lista', () => {
+  const { getByLabelText, getByText } = render(<App />);
+  const inputValue = 'Prueba 2';
+  userEvent.type(getByLabelText('Que necesitas?'), inputValue);
+  expect(screen.getByDisplayValue(inputValue)).toHaveValue(inputValue);
 });
